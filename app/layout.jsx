1bd67e758e6bb1e6a3e3f8aeb99fe21864711af0
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-06-24 14:00:35 +0200

    :first_quarter_moon: set up done

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app"></main>
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-
-const page = () => {
-  return <div></div>;
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & Share</h1>
+      <br className="max-md:hidden" />
+      <span>AI-Powered Prompts</span>
+    </section>
+  );
 };
 
-export default page;
+export default Home;
