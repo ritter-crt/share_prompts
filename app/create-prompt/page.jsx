cac69b2db8e540ab2e28fcd1a887a5e3c60d214e
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-07-04 10:56:15 +0200

    resolve merge conflicts

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,6 +1,39 @@
 import Feed from '@components/Feed';
 
-const Home = () => {
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+  const handleCreatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    // passing data from frontend to api endpoint using a post request
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify(post)({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
