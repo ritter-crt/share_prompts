6bb8686af136578b36d4abe1c9b6cd29cd0b330f
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-07-04 10:51:26 +0200

    frontend api call

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,26 +1,20 @@
-'use client';
+import Feed from '@components/Feed';
 
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-
-import Form from '@components/Form';
-const CreatePrompt = () => {
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  });
-  const handleCreatePrompt = async (e) => {};
+const Home = () => {
   return (
-    <Form
-      type="Create"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={handleCreatePrompt}
-    />
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      <Feed />
+    </section>
   );
 };
 
-export default CreatePrompt;
+export default Home;
