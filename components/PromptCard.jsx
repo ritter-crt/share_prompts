84771475a83ed5d15e43bcd98574b8c448ef53b0
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-07-05 16:01:21 +0200

    :flower_playing_cards: PromptCard done

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,10 +5,18 @@ import Image from 'next/image';
 
 import { usePathname, useRouter } from 'next/navigation';
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState('');
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied('', 3000));
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div>
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
             alt="use_image"
@@ -16,9 +24,34 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             height={40}
             className="rounded-full object-contain"
           />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? '/assets/icons/copy.svg'
+                : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          ></Image>
         </div>
       </div>
-      PromptCard
+      <p className="my-4 font-sa text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
     </div>
   );
 };
